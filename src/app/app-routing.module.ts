a1f79eda51a3abc795f05cfa87e132d1a941ac60
reussiteforever <khafo5852@gmail.com>
2023-05-16 09:27:24 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContratComponent } from './contrat/contrat.component';
+import { StageComponent } from './stage/stage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"dashboard", component:DashboardComponent},
+  {path:"contrat", component:ContratComponent},
+  {path:"stage", component:StageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContratComponent } from './contrat/contrat.component';
+import { StageComponent } from './stage/stage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    DashboardComponent,
+    SidebarComponent,
+    ContratComponent,
+    StageComponent
   ],
   imports: [
     BrowserModule,
