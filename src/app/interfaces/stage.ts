089bd15114941b4c4821120ad1dd0164cd59f379
reussiteforever <khafo5852@gmail.com>
2023-05-17 09:04:05 +0000

    Stage interface added

---
+++ src/app/interfaces/stage.ts
@@ -0,0 +1,11 @@
+export interface Stage {
+    id: number,
+    dateDebut: Date,
+    dateFin: Date,
+    responsableStage: string,
+    idTypeStage: number,
+    idPersonne: number,
+    idService: number,
+    idFonction: number,
+    state: number
+}
